c646b3089b16d77c87bc9c4ee9c8882916ddd5d6
Maksim Borisov <maksim.i.borisov@gmail.com>
2024-01-08 19:12:55 +0300

    FIX TreeBuilder construct

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('multifinger_declension');
+        $treeBuilder = new TreeBuilder('multifinger_declension');
 
         return $treeBuilder;
     }
